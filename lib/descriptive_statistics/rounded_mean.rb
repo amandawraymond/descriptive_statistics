74c195768ebb0e67aeca80b1153e9a4d191939a2
amandawraymond <amandawraymond@gmail.com>
2015-01-27 02:06:34 -0500

    filled rounded_mean

---
+++ lib/descriptive_statistics/rounded_mean.rb
@@ -0,0 +1,8 @@
+module DescriptiveStatistics
+  def rounded_mean(collection = self, &block)
+    values = Support::convert(collection, &block)
+    return DescriptiveStatistics.mean_empty_collection_default_value if values.empty?
+
+    (values.sum / values.number).round(4)
+  end
+end
